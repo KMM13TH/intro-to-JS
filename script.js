09fa0b2acd94ce9fd9af2d15d0cd54ab1c595797
sandhyakamath <sandhya_kammath@yahoo.co.in>
2024-12-14 18:53:27 +0000

    JS intro activity completed

---
+++ script.js
@@ -0,0 +1,20 @@
+document.getElementById("myheading2").innerHTML = "This my heading 2 from JavaScript."; // Statement 1
+
+document.getElementById("myparagraph").innerHTML = "this is paragraph from JavaScript"; // Statement 2
+
+var x = 10;
+var y = 12;
+var z = x + y;
+
+document.getElementById("add").innerHTML = z;
+
+var a = 25;
+var b = 18;
+
+document.getElementById("difference").innerHTML = a - b;
+
+var n = 12;
+var m = 7;
+var l = 12 * 7;
+
+document.getElementById("Product").innerHTML = l
